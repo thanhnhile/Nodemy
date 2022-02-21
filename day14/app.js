1258a09af928dca5fa1ad05548bb964669abb35a
thanhnhile <marrythanhnhipl205@gmail.com>
2022-02-21 15:43:03 +0700

    day14

---
+++ day14/app.js
@@ -0,0 +1,52 @@
+const imgFeature = document.querySelector('.img-feater img')
+const btnPrev = document.querySelector('.control.prev')
+const btnNext = document.querySelector('.control.next')
+const listImg = document.querySelectorAll('li img')
+
+function setShowImg(){
+    var showImage = listImg[currentIndex]
+    imgFeature.src = showImage.src
+    document.querySelector('li.active').classList.remove('active')
+    showImage.parentElement.classList.add('active')
+}
+//event listener
+listImg.forEach((img,index)=>{
+    img.addEventListener('click',function(){
+        currentIndex = index
+        setShowImg()
+    
+    })
+})
+btnPrev.addEventListener('click',()=>{
+    currentIndex = currentIndex === 0 ? listImg.length-1 : currentIndex-1
+    imgFeature.style.animation=''
+    setTimeout(()=>{
+        setShowImg()
+        imgFeature.style.animation='slideRight 1s ease-in-out forwards'
+    },1000)
+})
+btnNext.addEventListener('click',()=>{
+    currentIndex = currentIndex === listImg.length-1 ? 0 : currentIndex+1
+    imgFeature.style.animation=''
+    setTimeout(()=>{
+        setShowImg()
+        imgFeature.style.animation='slideLeft 1s ease-in-out forwards'
+    },2000)
+})
+function autoSlideShow(){
+    setInterval(()=>{
+        currentIndex+=1
+        if(currentIndex===listImg.length){
+            currentIndex=0
+        }
+        imgFeature.style.animation=''
+        setTimeout(()=>{
+            setShowImg()
+            imgFeature.style.animation='slideRight 1s ease-in-out forwards'
+        },2000)
+
+    },3000)
+}
+var currentIndex = 0
+setShowImg()
+autoSlideShow()
